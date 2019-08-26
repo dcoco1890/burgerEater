c5f817fab0b1064f3801dd1d4b6d1b8165a7ec79
Cohen <dcoco91@gmail.com>
2019-08-26 13:59:46 -0400

    added a line of code that hides the parent object when you click the devour button. This makes it disappear faster than just a location reload

--- public/assets/js/burger_script.js
+++ public/assets/js/burger_script.js
@@ -1,17 +1,20 @@
 $(document).ready(function() {
 
     function moveBurger(id) {
+
         $.ajax({
                 method: "PUT",
                 url: "/api/burgers/" + id
             })
             .then(function() {
+
                 location.reload();
             });
     }
 
     $(".devour").on("click", function() {
         var id = $(this).attr("data-id");
+        $(this).parent().hide();
         moveBurger(id);
     });
 
